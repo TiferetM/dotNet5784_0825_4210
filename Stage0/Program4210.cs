ac5f79e4d1e1412f4a69da3b5d3551dd1adbf671
Shulamit <s0583283644@gmail.com>
2023-10-26 23:04:49 +0300

    shulamit change

---
+++ Stage0/Program4210.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Targil0
+{
+    partial class Program
+    {
+        static partial void Welcome4210()
+        {
+            Console.WriteLine("I am also here!");
+        }
+    }
+}‏‏
