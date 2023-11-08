d810b455bca5aadd45909a8494cd50f4cbdf5314
Shulamit <s0583283644@gmail.com>
2023-11-09 00:13:55 +0200

    push starat stage9

--- DalTest/Program.cs
+++ DalTest/Program.cs
@@ -1,10 +1,16 @@
-﻿namespace DalTest
+﻿namespace DalTest;
+
+internal class Program
 {
-    internal class Program
+    try{
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
+       private static IDependency? s_dalDependency = new DependencyImplementation(); //stage 1
+    private static Engineer? s_dalEngineer = new EngineerIlementation(); //stage 1
+    private static ITask? s_dalTask = new TaskImplementation(); //stage 1
+    Initialization.Do(s_dalDependency, s_dalEngineer, s_dalTask);
+
+catch{
+        Console.WriteLine(e.Message);
 }
+
