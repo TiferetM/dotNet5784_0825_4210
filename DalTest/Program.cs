8fe90c30646940411eb654fd54915edd63594905
Shulamit <s0583283644@gmail.com>
2023-11-09 18:44:53 +0200

    try pull start stage9

--- DalTest/Program.cs
+++ DalTest/Program.cs
@@ -1,16 +1,10 @@
-﻿namespace DalTest;
-
-internal class Program
+﻿namespace DalTest
 {
-    try{
-    static void Main(string[] args)
+    internal class Program
     {
-       private static IDependency? s_dalDependency = new DependencyImplementation(); //stage 1
-    private static Engineer? s_dalEngineer = new EngineerIlementation(); //stage 1
-    private static ITask? s_dalTask = new TaskImplementation(); //stage 1
-    Initialization.Do(s_dalDependency, s_dalEngineer, s_dalTask);
-
-catch{
-        Console.WriteLine(e.Message);
+        static void Main(string[] args)
+        {
+           ז
+        }
+    }
 }
-
